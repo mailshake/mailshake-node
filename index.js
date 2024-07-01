c863b9a294ce652f13751879826444c9519e5b27
Josh Sherman <joshsherman@gmail.com>
2024-07-01 16:55:45 -0500

    chore: shore up linting issues

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-let Mailshake = require('./lib/mailshake').Mailshake;
+const Mailshake = require('./lib/mailshake').Mailshake;
 module.exports = Mailshake;
 module.exports.pushHandlerExpress = require('./lib/push-handler').pushHandlerExpress;
 module.exports.resolvePush = require('./lib/push-handler').resolvePush;
